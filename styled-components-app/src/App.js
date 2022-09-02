dfc15ed1f6f3b073aaebfaf8442bada4901953ac
HamInGyu <hfire1207@gmail.com>
2022-09-02 15:14:19 +0900

    [styled-components-app] #2.3 as를 활용해서 컴포넌트 스타일 그대로 두고 태그를 바꾸기

--- styled-components-app/src/App.js
+++ styled-components-app/src/App.js
@@ -3,19 +3,18 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+  font-size: 1rem;
 `;
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Btn>button태그 버튼</Btn>
+      <Btn as="a">a태그 버튼</Btn>
     </Father>
   );
 }
