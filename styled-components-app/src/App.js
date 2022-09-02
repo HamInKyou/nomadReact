5649463a0d0a6c627ef81f8a8e4d25add3d0fc7c
HamInGyu <hfire1207@gmail.com>
2022-09-02 15:15:45 +0900

    [styled-components-app] #2.3 스타일 컴포넌트에 default로 태그 속성 정의하기

--- styled-components-app/src/App.js
+++ styled-components-app/src/App.js
@@ -1,20 +1,17 @@
 import styled from "styled-components";
-
 const Father = styled.div`
   display: flex;
 `;
-const Btn = styled.button`
-  color: white;
+
+const Input = styled.input.attrs({ required: true })`
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-  font-size: 1rem;
 `;
+
 function App() {
   return (
-    <Father>
-      <Btn>button태그 버튼</Btn>
-      <Btn as="a">a태그 버튼</Btn>
+    <Father as="header">
+      <Input />
+      <Input />
     </Father>
   );
 }
