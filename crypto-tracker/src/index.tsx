3909ffd8cef32443d33936b1ae06023c31cb3eba
HamInGyu <hfire1207@gmail.com>
2022-09-09 20:56:09 +0900

    [crypro-tracker] #4.9 최상위(index)에 QueryClientProvider 세팅하기

--- crypto-tracker/src/index.tsx
+++ crypto-tracker/src/index.tsx
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    {/* QueryClientProvider로 감싼 내부에 있는 애들은 모두 props로 전달한 client에 접근 가능하다! */}
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
