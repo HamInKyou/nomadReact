b15841e642771223bc22dbf685321669f68dbba7
HamInGyu <hfire1207@gmail.com>
2022-09-13 20:30:29 +0900

    [todolistapp] #6.0 투두리스트앱 초기 세팅

--- todolistapp/src/App.tsx
+++ todolistapp/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import GlobalStyle from "./GlobalStyle";
+import ToDoList from "./TodoList";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
