713fefcdb72944d4871e621e49290a7393081167
HamInGyu <hfire1207@gmail.com>
2022-09-14 19:13:10 +0900

    [todolistapp] #6.1 validation 찍어먹어보기, required, minLength

--- todolistapp/src/TodoList.tsx
+++ todolistapp/src/TodoList.tsx
@@ -12,14 +12,38 @@ function ToDoList() {
   return (
     <div>
       {/* handleSubmit에서 유효성검사를 진행, 문제 없을 경우 onValid 실행 */}
-      <form onSubmit={handleSubmit(onValid)}>
-        {/* register 함수를 통해 각 input을 등록해줬다. */}
-        <input {...register("email")} placeholder="Email" />
-        <input {...register("First Name")} placeholder="First Name" />
-        <input {...register("Last Name")} placeholder="Last Name" />
-        <input {...register("Username")} placeholder="Username" />
-        <input {...register("Password")} placeholder="Password" />
-        <input {...register("PasswordConf")} placeholder="PasswordConf" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        {/*
+        validation 해보기
+        두번째 인자로 여러 validation 옵션 적용시켜줄 수 있다.
+        validation에 걸리면 해당 input으로 자동으로 포커싱되게 해준다!
+        validation에 걸리기 때문에 onValid 호출 안함!
+        */}
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("First Name", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("Last Name", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          // 최소 10글자 이상 되게 validation 주는 방법
+          {...register("Username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("Password", { required: true, minLength: 5 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("PasswordConf", { required: true, minLength: 5 })}
+          placeholder="PasswordConf"
+        />
         <button>Add</button>
       </form>
     </div>
