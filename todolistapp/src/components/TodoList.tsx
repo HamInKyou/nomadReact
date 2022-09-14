072f5d923f3fda8a2956f98ce202c1c75a28c303
HamInGyu <hfire1207@gmail.com>
2022-09-15 00:50:33 +0900

    [todolistapp] #6.6 todo입력 폼 컴포넌트 분리, atom 분리

--- todolistapp/src/components/TodoList.tsx
+++ todolistapp/src/components/TodoList.tsx
@@ -1,46 +1,17 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { atom, useRecoilState } from "recoil";
-
-interface IForm {
-  toDo: string;
-}
-
-interface IToDo {
-  text: string;
-  id: number;
-  category: "TO_DO" | "DOING" | "DONE";
-}
-
-const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
+import { useRecoilValue } from "recoil";
+import { toDoState } from "../atoms";
+import CreateToDo from "./CreateToDo";
 
 function ToDoList() {
   //useRecoilState는 useState훅 처럼, 상태와 상태를 변경할 수 있는 함수를 리턴
-  const [toDos, setToDos] = useRecoilState(toDoState);
-  const { register, handleSubmit, setValue } = useForm<IForm>();
-  const onSubmit = ({ toDo }: IForm) => {
-    setToDos((oldToDos) => [
-      { text: toDo, id: Date.now(), category: "TO_DO" },
-      ...oldToDos,
-    ]);
-    setValue("toDo", "");
-  };
+  const toDos = useRecoilValue(toDoState);
+
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register("toDo", {
-            required: "Please write a To Do",
-          })}
-          placeholder="Write a to do"
-        />
-        <button>Add</button>
-      </form>
+      <CreateToDo />
       <ul>
         {toDos.map((toDo) => (
           <li key={toDo.id}>{toDo.text}</li>
