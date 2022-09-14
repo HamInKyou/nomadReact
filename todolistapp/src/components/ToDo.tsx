01f360616375bb30f446d8b87eac6e9ed41fde74
HamInGyu <hfire1207@gmail.com>
2022-09-15 01:12:10 +0900

    [todolistapp] #6.7 todo onClick 이벤트 인자 받는 방식 수정

--- todolistapp/src/components/ToDo.tsx
+++ todolistapp/src/components/ToDo.tsx
@@ -1,21 +1,31 @@
+import React from "react";
 import { IToDo } from "../atoms";
 
 function ToDo({ text, category }: IToDo) {
   //interface["속성"]을 통해서 특정 속성의 타입을 뽑아낼 수 있다.
-  const onClick = (newCategory: IToDo["category"]) => {
-    console.log("i wanna to ", newCategory);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    console.log("i wanna to ", name);
   };
   return (
     <li>
       <span>{text}</span>
       {category !== "DOING" && (
-        <button onClick={() => onClick("DOING")}>Doing</button>
+        <button name="DOING " onClick={onClick}>
+          Doing
+        </button>
       )}
       {category !== "TO_DO" && (
-        <button onClick={() => onClick("TO_DO")}>To Do</button>
+        <button name="TO_DO " onClick={onClick}>
+          To Do
+        </button>
       )}
       {category !== "DONE" && (
-        <button onClick={() => onClick("DONE")}>Done</button>
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
       )}
     </li>
   );
