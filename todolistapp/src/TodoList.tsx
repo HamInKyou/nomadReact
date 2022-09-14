a17519c39d3ffbee92a3c87462229175aa080740
HamInGyu <hfire1207@gmail.com>
2022-09-14 18:54:05 +0900

    [todolistapp] #6.1 react-hook-form 연습, input 등록하기

--- todolistapp/src/TodoList.tsx
+++ todolistapp/src/TodoList.tsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setToDo(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(toDo);
-  };
+  //register는 input요소를 Form에 등록시키는 함수, input에 대한 name, 이벤트 핸들러 등 리턴
+  //watch는 등록된 input요소들의 변경사항을 객체로 묶어 리턴해주는 함수
+  const { register, watch } = useForm();
+  console.log(watch());
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} placeholder="Write a to do" />
+      <form>
+        {/* register 함수를 통해 각 input을 등록해줬다. */}
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("First Name")} placeholder="First Name" />
+        <input {...register("Last Name")} placeholder="Last Name" />
+        <input {...register("Username")} placeholder="Username" />
+        <input {...register("Password")} placeholder="Password" />
+        <input {...register("PasswordConf")} placeholder="PasswordConf" />
         <button>Add</button>
       </form>
     </div>
