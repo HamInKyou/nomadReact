36ae858b0a809d0be3bd989e801d2ce0a014eb32
HamInGyu <hfire1207@gmail.com>
2022-09-15 20:27:57 +0900

    [framer-motion-practice-app]#7.1 테마, 폰트, 글로벌스타일 세팅 완료

--- framer-motion-practice-app/src/index.tsx
+++ framer-motion-practice-app/src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./theme";
+import GlobalStyle from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
