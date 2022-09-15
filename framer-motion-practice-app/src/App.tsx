0b7895a1388e96c76b52db675a49d0fb6132a435
HamInGyu <hfire1207@gmail.com>
2022-09-15 21:50:29 +0900

    [framer-motion-practice-app]#7.4 자식 컴포넌트 애니메이션을 부모 컴포넌트 variants에서 조작하기

--- framer-motion-practice-app/src/App.tsx
+++ framer-motion-practice-app/src/App.tsx
@@ -13,23 +13,66 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Circle = styled(motion.div)`
   background-color: white;
-  border-radius: 10px;
+  height: 70px;
+  width: 70px;
+  place-self: center;
+  border-radius: 35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 // 애니메이션과 관련된 '설정'을 분리된 오브젝트에 작성
-const myVars = {
-  //각 속성들의 이름은 아무거나 상관없음.
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring" } },
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5, //0.5초 딜레이 후에 자식 컴포넌트 애니메이션 실행
+      staggerChildren: 0.2, //자식들에 차례대로 0.2초씩 딜레이를 줘서 순차적으로 애니메이션 실행하게
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 function App() {
   return (
     <Wrapper>
-      {/* 어떤 variants를 쓸건지, 그 variants의 속성들 중에 어떤걸 가져다 쓸건지 */}
-      <Box variants={myVars} initial="start" animate="end" />
+      {/* 
+        motion은 기본적으로 부모의 initial, animate 등을 자식에게 넘겨준다. 
+        즉, 자식들도 initial="start" animate="end"라는 속성을 갖고있는 것.
+        자식들에 동일한 속성 이름으로 속성에 다른 옵션을 주면, 자식은 그 옵션을 따른다.
+      */}
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
