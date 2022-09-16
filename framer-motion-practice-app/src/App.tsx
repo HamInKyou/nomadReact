cb38193486617bb0659395931b18d535334b731a
HamInGyu <hfire1207@gmail.com>
2022-09-17 00:28:58 +0900

    [framer-motion-practice-app]#7.16 layout 속성 활용해서 레이아웃의 변화가 있을 시 애니메이션
    레이아웃의 변화가 있을 시, 애니메이션을 자연스럽게 흐르듯이 이어준다!

--- framer-motion-practice-app/src/App.tsx
+++ framer-motion-practice-app/src/App.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
@@ -13,71 +13,36 @@ const Wrapper = styled(motion.div)`
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-  //custom값을 받아서 variants에 적용하려면, 이전의 객체 형식이 아니라 함수 형식으로 작성되어야해!
-  entry: (isBack: boolean) => ({
-    //custom에서 지정한 값을 인자로 받는다.
-    x: isBack ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  exit: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: { duration: 0.3 },
-  }),
-};
+const Circle = styled(motion.div)`
+  background-color: #00a5ff;
+  height: 100px;
+  width: 100px;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
 function App() {
-  const [visible, setVisible] = useState(1); //현재 차례의 슬라이드
-  const [back, setBack] = useState(false); //슬라이드 어디 방향으로 이동시킬지에 대한 state
-  const nextPlease = () => {
-    setBack(false); //back이 false니까 오른쪽에서 새로운게 나오는 방향으로
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-  const prevPlease = () => {
-    setBack(true); //back이 true니까 왼쪽에서 새로운게 나오는 방향으로
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper>
-      {/* Box exit할 때 수행하는 애니메이션에 대한 variants에 custom 전해주려면 exit을 발생시키는 AnimatePresence에 custom을 전달해줘야한다! */}
-      <AnimatePresence custom={back}>
-        <Box
-          custom={back} //variants에 custom 전달하여 동적으로 애니메이션 조작할 수 있게 해주었다!
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-          //리액트 컴포넌트는 key가 바뀔 때마다 새로운 컴포넌트라고 생각하고 기존걸 언마운트 시키고 새걸 새롭게 마운트시킨다!
-          //즉, key가 바뀌면 initial, animate, exit를 모두 수행한다는 것!
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={nextPlease}>next</button>
-      <button onClick={prevPlease}>prev</button>
+    <Wrapper onClick={toggleClicked}>
+      <Box
+        //Wrapper 클릭에 따라 clicked state가 바뀌며, 이에따라 레이아웃을 동적으로 변하게 함
+        style={{
+          justifyContent: clicked ? "center" : "flex-start",
+          alignItems: clicked ? "center" : "flex-start",
+        }}
+      >
+        {/* layout 속성을 부여하면, 해당 컴포넌트의 레이아웃이 바뀌었을 때(위치변경시) 애니메이션으로 자연스럽게 이어준다. */}
+        <Circle layout />
+      </Box>
     </Wrapper>
   );
 }
