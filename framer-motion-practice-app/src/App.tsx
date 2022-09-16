a0e68d0c5c0ccaafc1524a08bd554f9d23cc994f
HamInGyu <hfire1207@gmail.com>
2022-09-16 19:07:51 +0900

    [framer-motion-practice-app]#7.11 브라우저 스크롤 따라 박스 크기 조절하기

--- framer-motion-practice-app/src/App.tsx
+++ framer-motion-practice-app/src/App.tsx
@@ -3,7 +3,7 @@ import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
 import { useEffect } from "react";
 
 const Wrapper = styled(motion.div)`
-  height: 500vh;
+  height: 200vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -21,9 +21,7 @@ const Box = styled(motion.div)`
 function App() {
   const boxX = useMotionValue(0);
   const boxRotateZ = useTransform(boxX, [-800, 800], [-360, 360]);
-  //useScroll은 스크롤 상태(State)를 반환하는 훅
-  //scrollX/Y는 X/Y축으로 스크롤한 픽셀값, scrollX/YProgress는 스크롤 끝을 1로 보았을 때, 0부터 시작하여 X/Y축으로 어느정도 스크롤 했는지!
-  const { scrollY, scrollYProgress } = useScroll();
+  const { scrollYProgress } = useScroll();
   const gradient = useTransform(
     boxX,
     [-800, 800],
@@ -32,15 +30,14 @@ function App() {
       "linear-gradient(135deg, rgb(0, 238, 155), rgb(238, 178, 0))",
     ]
   );
-  useEffect(() => {
-    boxX.onChange(() => console.log(boxX.get()));
-  }, [boxX]);
-  useEffect(() => {
-    scrollY.onChange(() => console.log(scrollY.get(), scrollYProgress.get()));
-  }, [scrollY, scrollYProgress]);
+  const boxScale = useTransform(scrollYProgress, [0, 1], [1, 5]);
   return (
     <Wrapper style={{ background: gradient }}>
-      <Box style={{ x: boxX, rotateZ: boxRotateZ }} drag="x" dragSnapToOrigin />
+      <Box
+        style={{ x: boxX, rotateZ: boxRotateZ, scale: boxScale }}
+        drag="x"
+        dragSnapToOrigin
+      />
     </Wrapper>
   );
 }
