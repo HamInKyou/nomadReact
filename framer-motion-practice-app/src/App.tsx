4eaa012eab4b975a4f6c70a24bc8357a138e6c3a
HamInGyu <hfire1207@gmail.com>
2022-09-16 17:28:47 +0900

    [framer-motion-practice-app]#7.7 MotionValue로 요소 변형된 정도 추적하기

--- framer-motion-practice-app/src/App.tsx
+++ framer-motion-practice-app/src/App.tsx
@@ -1,20 +1,10 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -28,26 +18,26 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  hover: { rotateZ: 90 }, //호버하면 90도 회전하게
-  click: { borderRadius: "100px" }, //클릭하는동안 동그래지게
-};
-
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  //useMotionValue를 통해 시시각각 바뀌는 motion 컴포넌트의 스타일값을 캐치할 수 있다.
+  //이 값은 state가 아니다, 즉 해당 값이 변경되어도 리렌더링 되지 않는다!
+  //따라서 이 변경값을 추적하려면 다른 방법이 필요하다.
+  const boxX = useMotionValue(0);
+
+  //useMotionValue를 통해 얻은 객체에 onChange 이벤트달 수 있다.
+  //.get()을 통해 변한 값을 받을 수 있다!
+  useEffect(() => {
+    boxX.onChange(() => console.log(boxX.get()));
+  }, [boxX]);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin //드래그 손 떼면 제일 처음 본래 위치로 돌아가게
-          dragElastic={0.5} //범위 밖으로 땅길 수 있는 힘, 1이면 드래그 하는대로 밖으로 끌어지고, 0이면 범위 밖으로 절대 안끌려짐(설정 안한 default값은 0.5, 0~1사이의 값)
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover" //호버하면 실행할 에니메이션
-          whileTap="click" //클릭하는 동안 실행할 애니메이션
-        />
-      </BiggerBox>
+      {/* 
+        motion 컴포넌트는 향상된 style props가 있다.
+        간단히 style props에 속성을 부여하여 변형시킬 수 있다.
+        참고 : https://www.framer.com/docs/component/##transform
+        여기선 useMotionValue를 통해 boxX를 만들었고, x값을 추적하고, 변경할 수 있게 만들었다. 
+      */}
+      <Box style={{ x: boxX }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
