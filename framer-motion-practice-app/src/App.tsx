3dd8e6c2352e825abc224208b4232f762ac71f6a
HamInGyu <hfire1207@gmail.com>
2022-09-16 17:31:06 +0900

    [framer-motion-practice-app]#7.7 MotionValue로 요소 변형값 바로 세팅하는 방법 알아보기

--- framer-motion-practice-app/src/App.tsx
+++ framer-motion-practice-app/src/App.tsx
@@ -19,24 +19,19 @@ const Box = styled(motion.div)`
 `;
 
 function App() {
-  //useMotionValue를 통해 시시각각 바뀌는 motion 컴포넌트의 스타일값을 캐치할 수 있다.
-  //이 값은 state가 아니다, 즉 해당 값이 변경되어도 리렌더링 되지 않는다!
-  //따라서 이 변경값을 추적하려면 다른 방법이 필요하다.
   const boxX = useMotionValue(0);
 
-  //useMotionValue를 통해 얻은 객체에 onChange 이벤트달 수 있다.
-  //.get()을 통해 변한 값을 받을 수 있다!
-  useEffect(() => {
-    boxX.onChange(() => console.log(boxX.get()));
-  }, [boxX]);
   return (
     <Wrapper>
-      {/* 
-        motion 컴포넌트는 향상된 style props가 있다.
-        간단히 style props에 속성을 부여하여 변형시킬 수 있다.
-        참고 : https://www.framer.com/docs/component/##transform
-        여기선 useMotionValue를 통해 boxX를 만들었고, x값을 추적하고, 변경할 수 있게 만들었다. 
-      */}
+      <button
+        onClick={() => {
+          // .set으로 바로 변형 줄 수 있음
+          // 변형 사이에 애니메이션 없이 바로 뚝 바뀜
+          boxX.set(200);
+        }}
+      >
+        click me
+      </button>
       <Box style={{ x: boxX }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
