7f7ba17e777cf5eea9992696e9608d9c1cc91378
HamInGyu <hfire1207@gmail.com>
2022-09-16 19:03:39 +0900

    [framer-motion-practice-app]#7.10 브라우저 스크롤 상태 알려주는 MotionValue 만들기

--- framer-motion-practice-app/src/App.tsx
+++ framer-motion-practice-app/src/App.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
 import { useEffect } from "react";
 
 const Wrapper = styled(motion.div)`
-  height: 100vh;
+  height: 500vh;
   width: 100vw;
   display: flex;
   justify-content: center;
@@ -21,6 +21,9 @@ const Box = styled(motion.div)`
 function App() {
   const boxX = useMotionValue(0);
   const boxRotateZ = useTransform(boxX, [-800, 800], [-360, 360]);
+  //useScroll은 스크롤 상태(State)를 반환하는 훅
+  //scrollX/Y는 X/Y축으로 스크롤한 픽셀값, scrollX/YProgress는 스크롤 끝을 1로 보았을 때, 0부터 시작하여 X/Y축으로 어느정도 스크롤 했는지!
+  const { scrollY, scrollYProgress } = useScroll();
   const gradient = useTransform(
     boxX,
     [-800, 800],
@@ -32,6 +35,9 @@ function App() {
   useEffect(() => {
     boxX.onChange(() => console.log(boxX.get()));
   }, [boxX]);
+  useEffect(() => {
+    scrollY.onChange(() => console.log(scrollY.get(), scrollYProgress.get()));
+  }, [scrollY, scrollYProgress]);
   return (
     <Wrapper style={{ background: gradient }}>
       <Box style={{ x: boxX, rotateZ: boxRotateZ }} drag="x" dragSnapToOrigin />
