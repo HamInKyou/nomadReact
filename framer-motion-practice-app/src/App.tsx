a6fdbfbb9f8990acd8e298faf539704f4bc0f625
HamInGyu <hfire1207@gmail.com>
2022-09-17 00:38:15 +0900

    [framer-motion-practice-app]#7.16 layoutId 속성 활용해서 같은 컴포넌트라고 인지시키기
    다른 위치에 작성된 같은 컴포넌트들은 독립적으로 쓰이지만, layoutId를 활용하여 두 컴포넌트를 같은 컴포넌트라고 인지시켜 둘 사이의 차이에 따른 애니메이션을 자연스럽게 이어줄 수 있다!

--- framer-motion-practice-app/src/App.tsx
+++ framer-motion-practice-app/src/App.tsx
@@ -6,9 +6,8 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
@@ -17,6 +16,8 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   display: flex;
+  justify-content: center;
+  align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -24,7 +25,6 @@ const Circle = styled(motion.div)`
   background-color: #00a5ff;
   height: 100px;
   width: 100px;
-  border-radius: 50px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
@@ -33,15 +33,21 @@ function App() {
   const toggleClicked = () => setClicked((prev) => !prev);
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box
-        //Wrapper 클릭에 따라 clicked state가 바뀌며, 이에따라 레이아웃을 동적으로 변하게 함
-        style={{
-          justifyContent: clicked ? "center" : "flex-start",
-          alignItems: clicked ? "center" : "flex-start",
-        }}
-      >
-        {/* layout 속성을 부여하면, 해당 컴포넌트의 레이아웃이 바뀌었을 때(위치변경시) 애니메이션으로 자연스럽게 이어준다. */}
-        <Circle layout />
+      <Box>
+        {/* 
+          circle이라는 id로 layoutId를 공유함!
+          각각의 Circle 컴포넌트들은 다른 컴포넌트지만,
+          layoutId를 공유함으로써 framer-motion은 이 둘이 같은 컴포넌트라고 인지!
+          둘 사이의 차이를 에니메이션으로 자연스럽게 이어준다!
+        */}
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+        ) : null}
       </Box>
     </Wrapper>
   );
