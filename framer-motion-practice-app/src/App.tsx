25ddbfa9ec0c8c89ffbf5d2b7ee9b7cc449cf911
HamInGyu <hfire1207@gmail.com>
2022-09-17 00:16:40 +0900

    [framer-motion-practice-app]#7.15 custom활용하여 variants 동적으로 제어하기!

--- framer-motion-practice-app/src/App.tsx
+++ framer-motion-practice-app/src/App.tsx
@@ -26,43 +26,58 @@ const Box = styled(motion.div)`
 `;
 
 const box = {
-  invisible: {
-    x: 500,
+  //custom값을 받아서 variants에 적용하려면, 이전의 객체 형식이 아니라 함수 형식으로 작성되어야해!
+  entry: (isBack: boolean) => ({
+    //custom에서 지정한 값을 인자로 받는다.
+    x: isBack ? -500 : 500,
     opacity: 0,
     scale: 0,
-  },
-  visible: {
+  }),
+  center: {
     x: 0,
     opacity: 1,
     scale: 1,
     transition: {
-      duration: 0.5,
+      duration: 0.3,
     },
   },
-  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 0.5 } },
+  exit: (isBack: boolean) => ({
+    x: isBack ? 500 : -500,
+    opacity: 0,
+    scale: 0,
+    transition: { duration: 0.3 },
+  }),
 };
-
 function App() {
-  const [visible, setVisible] = useState(1);
-  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const [visible, setVisible] = useState(1); //현재 차례의 슬라이드
+  const [back, setBack] = useState(false); //슬라이드 어디 방향으로 이동시킬지에 대한 state
+  const nextPlease = () => {
+    setBack(false); //back이 false니까 오른쪽에서 새로운게 나오는 방향으로
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true); //back이 true니까 왼쪽에서 새로운게 나오는 방향으로
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
   return (
     <Wrapper>
-      <AnimatePresence>
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-          i === visible ? (
-            <Box
-              variants={box}
-              initial="invisible" //컴포넌트 마운트 되었을때 애나메이션 초기값 (해당 슬라이드 오른쪽에서 생겨남)
-              animate="visible" //애니메이션 마침점 (해당 슬라이드 중간으로 옴)
-              exit="exit" // 컴포넌트 언마운트 되기 전 ( 해당 슬라이드 왼쪽으로 사라짐)
-              key={i}
-            >
-              {i}
-            </Box>
-          ) : null
-        )}
+      {/* Box exit할 때 수행하는 애니메이션에 대한 variants에 custom 전해주려면 exit을 발생시키는 AnimatePresence에 custom을 전달해줘야한다! */}
+      <AnimatePresence custom={back}>
+        <Box
+          custom={back} //variants에 custom 전달하여 동적으로 애니메이션 조작할 수 있게 해주었다!
+          variants={box}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+          //리액트 컴포넌트는 key가 바뀔 때마다 새로운 컴포넌트라고 생각하고 기존걸 언마운트 시키고 새걸 새롭게 마운트시킨다!
+          //즉, key가 바뀌면 initial, animate, exit를 모두 수행한다는 것!
+        >
+          {visible}
+        </Box>
       </AnimatePresence>
       <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   );
 }
