61533e8c2f35a7caca22b814b8cc6fc086a25aa9
HamInGyu <hfire1207@gmail.com>
2022-09-16 21:19:58 +0900

    [framer-motion-practice-app]#7.13 컴포넌트 사라질 때 수행할 애니메이션 적용하는 방법
    AnimatePresence 활용하기

--- framer-motion-practice-app/src/App.tsx
+++ framer-motion-practice-app/src/App.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -9,39 +10,50 @@ const Wrapper = styled(motion.div)`
   align-items: center;
 `;
 
-//Svg 안에 html path요소 있으므로 nested css 적용해서!
-const Svg = styled.svg`
-  width: 300px;
-  height: 300px;
-  path {
-    stroke: white;
-    strike-width=2;
-  }
+const Box = styled(motion.div)`
+  width: 400px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  position: absolute;
+  top: 100px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const svgVariant = {
-  start: { pathLength: 0, fill: "rgba(255, 255, 255, 0)" },
-  end: {
-    fill: "rgba(255, 255, 255, 1)",
-    pathLength: 1,
+const boxVariants = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    rotateZ: 360,
+  },
+  leaving: {
+    opacity: 0,
+    scale: 0,
+    y: 50,
   },
 };
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const toggleShowing = () => setShowing((prev) => !prev);
   return (
     <Wrapper>
-      <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <motion.path
-          variants={svgVariant}
-          initial="start"
-          animate="end"
-          transition={{
-            default: { duration: 2 }, //모든 애니메이션 속성에 적용!
-            fill: { duration: 1, delay: 1 }, //특정 애니메이션에 따로 transition 주고싶을 때!
-          }}
-          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
-        />
-      </Svg>
+      <button onClick={toggleShowing}>Click</button>
+      {/* 컴포넌트 언마운트될때 애니메이션을 주려면 AnimatePresence 컴포넌트로 감싸고, 안에 조건부 렌더링 구문을 포함하자. */}
+      <AnimatePresence>
+        {showing ? (
+          <Box
+            variants={boxVariants}
+            initial="initial" //컴포넌트 마운트 되었을 떄 초기 상태
+            animate="visible" //애니메이션 끝 상태
+            exit="leaving" //AnimatePresence에 의해 사용되어질 수 있는 속성, 컴포넌트가 사라질 때 수행할 끝 애니메이션
+          />
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
