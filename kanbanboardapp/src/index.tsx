f0bc962191bbd336479bcfdec09d726ceecbdf8d
HamInGyu <hfire1207@gmail.com>
2022-09-20 19:12:20 +0900

    [kanbanboardapp]#8.0 theme interface 정의, themeProvider 세팅

--- kanbanboardapp/src/index.tsx
+++ kanbanboardapp/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
 import { Reset } from "styled-reset";
+import { ThemeProvider } from "styled-components";
+import { darkTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +13,9 @@ root.render(
   <React.StrictMode>
     <Reset />
     <RecoilRoot>
-      <App />
+      <ThemeProvider theme={darkTheme}>
+        <App />
+      </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>
 );
