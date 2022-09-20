2b23775ef6070b70e891459511b8470ea4afaa78
HamInGyu <hfire1207@gmail.com>
2022-09-20 19:07:44 +0900

    [kanbanboardapp]#8.0 styled-reset 설치

--- kanbanboardapp/src/index.tsx
+++ kanbanboardapp/src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import { Reset } from "styled-reset";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Reset />
     <RecoilRoot>
       <App />
     </RecoilRoot>
