deee19384ecad6fa9eafb0f513c3c94bce8bd97f
HamInGyu <hfire1207@gmail.com>
2022-09-20 20:46:46 +0900

    [kanbanboardapp]#8.3 테마 재정의

--- kanbanboardapp/src/theme.ts
+++ kanbanboardapp/src/theme.ts
@@ -1,8 +1,8 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#ffffff",
-  accentColor: "#9c88ff",
-  cardBgColor: "transparent",
+  textColor: "#000000",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
