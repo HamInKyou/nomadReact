b00dec6c5146329dd6407c4f36966bdaa40ed678
HamInGyu <hfire1207@gmail.com>
2022-09-20 20:42:09 +0900

    [kanbanboardapp]#8.2 드래그할 수 있게 세팅하기

--- kanbanboardapp/src/App.tsx
+++ kanbanboardapp/src/App.tsx
@@ -3,27 +3,42 @@ import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 function App() {
   const onDragEnd = () => {};
   return (
-    // DragDropContext는 드래그 앤 드롭을 가능하게 하고자 하는 영역
-    // onDragEnd는 필수로, 유저가 드래그를 끝낸 시점에 불려지는 함수(필수props)
     <DragDropContext onDragEnd={onDragEnd}>
       <div>
-        {/* 
-        Dropable은 우리가 드롭해서 놓을 수 있는 영역
-        Dropable의 자식은 element가 아니라 함수여야한다! 
-        */}
         <Droppable droppableId="one">
-          {() => (
-            <ul>
-              {/* 
-              Draggable은 우리가 드래그할 수 있는 요소 설정하는 것
-              Droggable의 자식은 element가 아니라 함수여야한다!
-              Draggable은 draggableId랑 index를 필수 props로 갖는다! 
-              */}
+          {/* 
+          Droppable의 자식으로 쓰이는 함수
+          첫번째 인자 magic -> 공식 예제에선 provided라고 썼지만, 이름 바꿔도 상관없다는걸 알 수 있다.
+          첫번째 인자 magic 객체는 innerRef와 droppableProps를 갖고 있다.
+          */}
+          {(magic) => (
+            <ul ref={magic.innerRef} {...magic.droppableProps}>
               <Draggable draggableId="first" index={0}>
-                {() => <li>One</li>}
+                {/* 
+                Draggable의 자식으로 쓰이는 함수
+                첫번째 인자 magic -> 공식 예제에선 provided라고 썼지만, 이름 바꿔도 상관없다는걸 알 수 있다.
+                첫번째 인자 magic 객체는 innerRef와 draggableProps와 dragHandleProps를 갖고 있다.
+                Draggable로 감싼 요소를 드래그하고 싶을 때, draggableProps를 할당해주어 드래그할 수 있게 설정한다.
+                Draggable로 감싼 요소 중에 ragHandleProps를 할당해주어야 그 요소를 잡고 드래그 할 수 있게 된다!(할당 안하면 드래그 안됨!)
+                */}
+                {(magic) => (
+                  <li
+                    ref={magic.innerRef}
+                    {...magic.draggableProps}
+                    {...magic.dragHandleProps}
+                  >
+                    <span>🔥</span>
+                    One
+                  </li>
+                )}
               </Draggable>
               <Draggable draggableId="second" index={1}>
-                {() => <li>Two</li>}
+                {(magic) => (
+                  <li ref={magic.innerRef} {...magic.draggableProps}>
+                    <span {...magic.dragHandleProps}>🔥</span>
+                    Two
+                  </li>
+                )}
               </Draggable>
             </ul>
           )}
