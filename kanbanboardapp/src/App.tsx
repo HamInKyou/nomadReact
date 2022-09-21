567f796529952590ba9024fef61629933185c2d4
HamInGyu <hfire1207@gmail.com>
2022-09-21 21:18:54 +0900

    [kanbanboardapp]#8.4~5 칸반보드 카드 옮기면 인덱스 옮기고 재정렬하게 만들기

--- kanbanboardapp/src/App.tsx
+++ kanbanboardapp/src/App.tsx
@@ -1,5 +1,12 @@
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
+import { toDoState } from "./atoms";
 
 const Wrapper = styled.div`
   display: flex;
@@ -32,10 +39,24 @@ const Card = styled.div`
   background-color: ${(props) => props.theme.cardColor};
 `;
 
-const toDos = ["a", "b", "c", "d", "e", "f"];
-
 function App() {
-  const onDragEnd = () => {};
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
+    //draggableId는 드래그하는 애의 draggableId
+    //destination은 드랍하는 곳의 정보 (droppableId, index 갖는다)
+    //source는 드래그 시작하는 곳의 정보 (droppableId, index 갖는다)
+    if (!destination) return; //목적지 없음 그냥 끝내기
+    setToDos((oldToDos) => {
+      const toDosCopy = [...oldToDos]; //불변성 위해 카피
+      // 1) Delete item on source.index 시작 위치의 요소 지우기
+      console.log("Delete item on", source.index);
+      toDosCopy.splice(source.index, 1); //index 위치에서 한개 제거
+      // 2) Put back the item on the destination.index // 종료 위치에 생성하기
+      toDosCopy.splice(destination?.index, 0, draggableId); //종료위치에 삽입
+      console.log(toDosCopy);
+      return toDosCopy;
+    });
+  };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
@@ -44,7 +65,8 @@ function App() {
             {(magic) => (
               <Board ref={magic.innerRef} {...magic.droppableProps}>
                 {toDos.map((toDo, index) => (
-                  <Draggable draggableId={toDo} index={index}>
+                  //draggable의 key는 draggableId랑 같아야한다!
+                  <Draggable draggableId={toDo} index={index} key={toDo}>
                     {(magic) => (
                       <Card
                         ref={magic.innerRef}
