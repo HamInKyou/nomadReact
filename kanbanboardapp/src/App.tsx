72717ef6ceb4a42dc14aecaf7376e8ca2619bf33
HamInGyu <hfire1207@gmail.com>
2022-09-22 15:11:23 +0900

    [kanbanboardapp]#8.10 다른 보드간에 드래그앤 드롭 다루기

--- kanbanboardapp/src/App.tsx
+++ kanbanboardapp/src/App.tsx
@@ -26,6 +26,7 @@ function App() {
   const onDragEnd = (info: DropResult) => {
     //목적지에 대한 정보, 드래그하는 요소의 정보, 시작지에 대한 정보
     const { destination, draggableId, source } = info;
+    if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       // 같은 보드 내에서 드래그앤 드롭했을 경우
       setToDos((allBoards) => {
@@ -39,6 +40,21 @@ function App() {
         return {
           ...allBoards,
           [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination.droppableId !== source.droppableId) {
+      // 다른 보드로 드래그앤 드롭했을경우
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]]; //시작 복사본 만들기
+        const destinationBoard = [...allBoards[destination.droppableId]]; //목적지 복사본 만들기
+        sourceBoard.splice(source.index, 1); //시작에서 지우고
+        destinationBoard.splice(destination?.index, 0, draggableId); //목적지에 추가하기
+        //지우고 추가한 복사본 보드들을 업데이트하기
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
         };
       });
     }
