b75fb3914f25c516352297ae84a4d381992c4a8b
HamInGyu <hfire1207@gmail.com>
2022-09-22 15:38:08 +0900

    [kanbanboardapp]#8.11 드랍할 수 있는 영역 크기 늘리기,  카드가 보드에서 떠나거나 들어올때 드랍영역 색 바꾸기
    snapshot 활용!

--- kanbanboardapp/src/Components/Board.tsx
+++ kanbanboardapp/src/Components/Board.tsx
@@ -9,12 +9,26 @@ const Wrapper = styled.div`
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
   min-height: 300px;
+  display: flex;
+  flex-direction: column;
 `;
 const Title = styled.h2`
   text-align: center;
   font-weight: 600;
   margin-bottom: 10px;
   font-size: 18px;
+`;
+
+interface IAreaProps {
+  isDraggingFromThis: boolean;
+  isDraggingOver: boolean;
+}
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "red" : "blue"};
+  flex-grow: 1; //드랍 가능한 영역 사이즈 보드 전체로 늘리기
+  transition: background-color 0.3s ease-in-out;
 `;
 
 interface IBoardProps {
@@ -27,9 +41,12 @@ function Board({ toDos, boardId }: IBoardProps) {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(magic) => (
-          <div
-            style={{ backgroundColor: "red" }}
+        {(magic, snapshot) => (
+          //snapshot.isDraggingOver  : 유저가 어떤 요소 드래그해서 해당 보드 집입했을시 활성화
+          //snapshot.draggingFromThisWith : 유저가 해당 보드로부터 드래그를 시작했는지?
+          <Area
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
             ref={magic.innerRef}
             {...magic.droppableProps}
           >
@@ -37,7 +54,7 @@ function Board({ toDos, boardId }: IBoardProps) {
               <DraggableCard key={toDo} index={index} toDo={toDo} />
             ))}
             {magic.placeholder}
-          </div>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
